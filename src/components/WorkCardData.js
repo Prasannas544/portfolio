5144ccec4843e4ba81558399639dbe4c101a35a9
prasannas544 <prasanna.swain@stepapp.ai>
2023-05-12 14:58:58 +0530

    quizApp added

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,39 +1,45 @@
-
-import utube from "../assets/youtube_clone.png"
-import food from "../assets/FoodApp.png"
-import regform from "../assets/RegForm.png"
-import hoobanks from "../assets/hoobanks.png"
+import utube from "../assets/youtube_clone.png";
+import food from "../assets/FoodApp.png";
+import regform from "../assets/RegForm.png";
+import hoobanks from "../assets/hoobanks.png";
+import quizAppimg from "../assets/quizAppimg.png";
 
 const ProjectCardData = [
-    {
-        imgsrc: food,
-        title: "FoodApp (android)",
-        text: "Developed a React Native app that allows users to discover and save recipes from around the world. Integrated with local bundle for recipe information, implemented bookmarking functionality using Redux Toolkit. A clean and organized code written with complete inline styling.",
-        view: "https://frolicking-pony-75db1c.netlify.app/",
-        source: "https://github.com/Prasannas544/FoodApp"
-    },
-    {
-        imgsrc: utube,
-        title: "Youtube Clone",
-        text: "The YouTube clone project is designed to provide a seamless user experience for video consumption, much like the popular YouTube website. Key features such as video playback and search functionalit are included using various APIs",
-        view: "https://lovely-vacherin-99da77.netlify.app/",
-        source: "https://github.com/Prasannas544/Youtube_clone"
-    },
-    {
-        imgsrc: regform,
-        title: "RegForm",
-        text: "A simple registration form using HTML, CSS, and plain JavaScript to collect user information for lead generation and to prevent cold calling. The form is designed to be visually appealing and user-friendly, with clear instructions and confirmation messages upon successful submission.",
-        view: "https://heroic-semolina-047009.netlify.app/",
-        source: "https://github.com/Prasannas544/Registration_Form"
-    },
-    {
-        imgsrc: hoobanks,
-        title: "Responsive webpage",
-        text: "A fully responsive webpage created using Vite, a fast and lightweight development tool that offers features like instant server startup, fast compilation. For styling, Tailwind CSS was used, a popular utility-first CSS framework that provides a comprehensive set of pre-built styling components and utilities, allowing developers to easily and quickly create custom designs",
-        view: "https://brilliant-queijadas-c6c913.netlify.app",
-        source: "https://github.com/Prasannas544/hoobanks"
-    },
+  {
+    imgsrc: food,
+    title: "FoodApp (android)",
+    text: "Developed a React Native app that allows users to discover and save recipes from around the world. Integrated with local bundle for recipe information, implemented bookmarking functionality using Redux Toolkit. A clean and organized code written with complete inline styling.",
+    view: "https://frolicking-pony-75db1c.netlify.app/",
+    source: "https://github.com/Prasannas544/FoodApp",
+  },
+  {
+    imgsrc: utube,
+    title: "Youtube Clone",
+    text: "The YouTube clone project is designed to provide a seamless user experience for video consumption, much like the popular YouTube website. Key features such as video playback and search functionalit are included using various APIs",
+    view: "https://lovely-vacherin-99da77.netlify.app/",
+    source: "https://github.com/Prasannas544/Youtube_clone",
+  },
+  {
+    imgsrc: hoobanks,
+    title: "Responsive webpage",
+    text: "A fully responsive webpage created using Vite, a fast and lightweight development tool that offers features like instant server startup, fast compilation. For styling, Tailwind CSS was used, a popular utility-first CSS framework that provides a comprehensive set of pre-built styling components and utilities, allowing developers to easily and quickly create custom designs",
+    view: "https://brilliant-queijadas-c6c913.netlify.app",
+    source: "https://github.com/Prasannas544/hoobanks",
+  },
+  {
+    imgsrc: quizAppimg,
+    title: "Kowalski QuizApp",
+    text: "A quiz application using React Native, featuring a comic-inspired user interface that delivers a visually-stimulating experience. The app's user experience is optimized for maximum enjoyment, with seamless interactions and intuitive navigation",
+    view: "https://frolicking-cassata-49da7b.netlify.app",
+    source: "https://github.com/Prasannas544/QuizApp_downlaodpage",
+  },
+  {
+    imgsrc: regform,
+    title: "RegForm",
+    text: "A simple registration form using HTML, CSS, and plain JavaScript to collect user information for lead generation and to prevent cold calling. The form is designed to be visually appealing and user-friendly, with clear instructions and confirmation messages upon successful submission.",
+    view: "https://heroic-semolina-047009.netlify.app/",
+    source: "https://github.com/Prasannas544/Registration_Form",
+  },
+];
 
-]
-
-export default ProjectCardData
+export default ProjectCardData;
